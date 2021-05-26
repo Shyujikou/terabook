d74308a6ced347d96c73f51c84a5940b986a1108
Ziheng Zhou <shyujikou@icloud.com>
2021-05-26 14:24:00 +0800

    Add panda js

---
+++ js/panda/ColorData.js
@@ -0,0 +1,15 @@
+(function(){this['色データ']=this['作る']();
+this['色データ']['登録']=dtlbind(this,function(数字たち){
+var self=this;var 自分=self;
+this['データ配列']=数字たち['split'](",");
+return this;
+});
+return this['色データ']['取得']=dtlbind(this,function(){
+var self=this;var 自分=self;
+return this['データ配列']['連結']()['加工'](dtlbind(this,function(e){
+var self=this;var 自分=self;
+return root['window']['parseInt']((e));
+}));
+});
+}).checkerror().apply(root,[]);
+//# sourceMappingURL=ColorData.js.map
